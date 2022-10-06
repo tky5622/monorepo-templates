525cec6e4d1d1a5c29bcbd6a95caa3a7dc2a8a5b
takuya <>
2022-10-06 13:42:33 +0900

    lll

--- libs/pixel-streaming-editor/src/lib/pixel-streaming-editor.spec.tsx
+++ libs/pixel-streaming-editor/src/lib/pixel-streaming-editor.spec.tsx
@@ -4,6 +4,7 @@ import PixelStreamingEditor from './pixel-streaming-editor'
 
 describe('PixelStreamingEditor', () => {
   it('should render successfully', () => {
+    // eslint-disable-next-line react/react-in-jsx-scope
     const { baseElement } = render(<PixelStreamingEditor />)
     expect(baseElement).toBeTruthy()
   })
