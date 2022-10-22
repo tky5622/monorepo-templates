6d08b6dacd616bde4823157cfa6b2a498e861f4d
takuya <>
2022-10-23 06:05:44 +0900

    fix: comment out

--- apps/front/src/pages/artist/[id].tsx
+++ apps/front/src/pages/artist/[id].tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { NftDetails } from '../../component/nft/NftDetails'
 import TabContainer from '../../component/nft/TabContainer'
-import UploadNFTButton from '../../component/nft/UploadNft'
+// import UploadNFTButton from '../../component/nft/UploadNft'
 import NoSSR from '../../component/shared/NoSSR'
 const Artist: NextPage = () => {
   return (
