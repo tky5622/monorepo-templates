36ad2d285b6d30b58c261d8a965c45b960b0602c
takuya <>
2022-10-29 06:38:21 +0900

    fix

--- apps/front/src/app/layout.tsx
+++ apps/front/src/app/layout.tsx
@@ -1,38 +1,39 @@
 'use client'
 /* eslint-disable no-constant-condition */
 import { ApolloProvider } from '@apollo/client'
-import { ColorScheme, ColorSchemeProvider } from '@mantine/core'
+// import { ColorScheme, ColorSchemeProvider } from '@mantine/core'
 import React, { PropsWithChildren } from 'react'
 import client from '../../apollo-client'
 import Layout from '../component/layout/Layout'
 import '../styles/globals.css'
 import '../styles/player.css'
 import RootStyleRegistry from './emotion'
+import Head from 'next/head'
 
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
   // const [colorScheme, setColorScheme] = useState<ColorScheme>('dark')
 
-  const toggleColorScheme = (value?: ColorScheme) =>
-    console.log(value || ('dark' === 'dark' ? 'dark' : 'dark'))
+  // const toggleColorScheme = (value?: ColorScheme) =>
+  //   console.log(value || ('dark' === 'dark' ? 'dark' : 'dark'))
 
   return (
     <html lang="en">
-      <head>
+      <Head>
         <title>Next.js</title>
-      </head>
+      </Head>
       <body>
           <RootStyleRegistry>
-          <ColorSchemeProvider
+          {/* <ColorSchemeProvider
               colorScheme={'dark'}
               toggleColorScheme={toggleColorScheme}
-          >
+          > */}
             <ApolloProvider client={client}>
             <Layout>
               {children}
             </Layout>
             </ApolloProvider>
 
-          </ColorSchemeProvider>
+          {/* </ColorSchemeProvider> */}
           </RootStyleRegistry>
       </body>
     </html>
